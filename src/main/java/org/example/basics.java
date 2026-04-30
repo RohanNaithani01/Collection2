e96d5ad27ed3f56072bcad1ea37840993764da3a
RohanNaithani01 <rohannaithani02@gmail.com>
2026-04-30 10:10:33 +0530

    Queue

--- src/main/java/org/example/basics.java
+++ src/main/java/org/example/basics.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.util.ArrayDeque;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.TreeSet;
 
 public class basics {
@@ -24,8 +26,27 @@ public class basics {
         ts.add(0);
         ts.add(-1);
         System.out.println(ts);
-        System.out.println(ts.floor(1));
-        System.out.println(ts.ceiling(2));
+        System.out.println(ts.floor(1)); // <= 1
+        System.out.println(ts.ceiling(2)); // >= 2
+        for(var num: ts){
+            System.out.println(num);
+        }
 
+        ArrayDeque<Integer> ad=new ArrayDeque<>();
+        ad.offer(2);
+        ad.offer(1);
+        ad.offer(4);
+        ad.offer(3);
+        for(var num:ad){
+            System.out.println(num);
+        }
+        System.out.println(ad);
+        System.out.println(ad.peek());
+        System.out.println(ad.pop());
+        System.out.println(ad);
+
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        pq.offer(1);
+        pq.offer(0);
     }
 }
